c85a128a05b0e197f1f6c2b2efa3649c2f4e0bd3
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-07-12 02:33:50 +0530

    changes

--- JunitTesting/src/test/java/com/ashish/JunitTesting/JunitTestingApplicationTests.java
+++ JunitTesting/src/test/java/com/ashish/JunitTesting/JunitTestingApplicationTests.java
@@ -1,13 +1,56 @@
 package com.ashish.JunitTesting;
 
-import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@Slf4j
 class JunitTestingApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@BeforeEach // run before each testnumber
+	void setUp(){
+		log.info("starting the method, setting up config");
 	}
+
+	@AfterEach // run after with each test
+	void tearDown(){
+		log.info("tearing down the method");
+	}
+
+	@BeforeAll
+	static void setUpOnce(){
+		log.info("setup once");
+	}
+
+	@AfterAll
+	static void tearDownOnce(){
+		log.info("tearing down once");
+	}
+	@Test
+	void testNumberOne() {
+		log.info("test one is run");
+		int a =5;
+		int b=4;
+		int result = addTwoNumbers(a,b);
+
+		Assertions.assertEquals(result,9);
+//		Assertions.assertThat(result)
+//				.isEqualTo(9)
+//				.isCloseTo(10, Offset.offset(1));
+
+	}
+
+	@Test
+//	@DisplayName("displaytest") // changes the name
+//	@Disabled // disabled the name
+	void testNumberTwo(){
+		log.info("test two is run");
+
+	}
+	int addTwoNumbers(int a, int b) {
+		return a+b;
+	}
+
 
 }
