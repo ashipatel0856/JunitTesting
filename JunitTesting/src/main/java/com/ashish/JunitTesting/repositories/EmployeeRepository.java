b65d99d08459d8f2bb7fbd9afb7292c166be12b1
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-07-12 13:15:40 +0530

    first

---
+++ JunitTesting/src/main/java/com/ashish/JunitTesting/repositories/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.ashish.JunitTesting.repositories;
+
+
+
+import com.ashish.JunitTesting.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    List<Employee> findByEmail(String email);
+}
