f4fac500d4fed93ea97e6d66432cf4fbbf441707
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-08-01 20:52:09 +0530

    changes

---
+++ JunitTesting/src/test/java/com/ashish/JunitTesting/controllers/AbstractIntegrationTest.java
@@ -0,0 +1,17 @@
+package com.ashish.JunitTesting.controllers;
+
+import com.ashish.JunitTesting.TestContainerConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@AutoConfigureWebTestClient(timeout = "100000")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestContainerConfiguration.class)
+public class AbstractIntegrationTest {
+
+    @Autowired
+    private WebTestClient webClient;
+}
--- JunitTesting/src/test/java/com/ashish/JunitTesting/controllers/EmployeeControllerTestIT.java
+++ JunitTesting/src/test/java/com/ashish/JunitTesting/controllers/EmployeeControllerTestIT.java
@@ -7,19 +7,16 @@ import com.ashish.JunitTesting.repositories.EmployeeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @AutoConfigureWebTestClient(timeout = "100000")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(TestContainerConfiguration.class)
-class EmployeeControllerTestIT {
+class EmployeeControllerTestIT extends AbstractIntegrationTest {
 
     @Autowired
     private WebTestClient webClient;
@@ -55,8 +52,10 @@ class EmployeeControllerTestIT {
                 .uri("/employees/{id}" + savedEmployee.getId())
                 .exchange()
                 .expectStatus().isOk()
-                .expectBody(EmployeeDto.class)
-                .isEqualTo(testEmployeeDto);
+                .expectBody()
+                .jsonPath("$.id").isEqualTo(savedEmployee.getId())
+                .jsonPath("$.email").isEqualTo(savedEmployee.getEmail());
+
 //                .value(employeeDto -> {
 //                    assertThat(employeeDto.getEmail()).isEqualTo(savedEmployee.getEmail());
 //                    assertThat(employeeDto.getId()).isEqualTo(savedEmployee.getId());
@@ -91,6 +90,65 @@ class EmployeeControllerTestIT {
                 .expectBody()
                 .jsonPath("$.email").isEqualTo(testEmployeeDto.getEmail())
                 .jsonPath("$.name").isEqualTo(testEmployeeDto.getName());
+
     }
 
+    @Test
+    void testUpdateEmployee_whenEmployeeDoesNotExists_thenThrowException() {
+        webTestClient.put()
+                .uri("/employees/999")
+                .bodyValue(testEmployeeDto)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    void testUpdateEmployee_whenAttemptingToUpdateTheEmail_thenThrowException() {
+       Employee savedEmployee = employeeRepository.save(employee);
+       testEmployeeDto.setName("Random Name");
+       testEmployeeDto.setEmail("random@gmail.com");
+       webTestClient.put()
+               .uri("/employees/{id}" + savedEmployee.getId())
+               .bodyValue(testEmployeeDto)
+               .exchange()
+               .expectStatus().is5xxServerError();
+    }
+
+    @Test
+    void testUpdateEmployee_whenEmployeeIsValid_thenUpdateEmployee() {
+        Employee savedEmployee = employeeRepository.save(employee);
+        testEmployeeDto.setName("Random Name");
+        testEmployeeDto.setSalary(250L);
+
+        webTestClient.put()
+                .uri("/employees/{id}" + savedEmployee.getId())
+                .bodyValue(testEmployeeDto)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(EmployeeDto.class)
+                .isEqualTo(testEmployeeDto);
+    }
+
+    @Test
+    void testDeleteEmployee_whenEmployeeDoesNotExists_thenThrowException() {
+        webTestClient.delete()
+                .uri("/employees/1")
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    void testDeleteEmployee_whenEmployeeIsValid_thenDeleteEmployee() {
+        Employee savedEmployee = employeeRepository.save(employee);
+        webTestClient.delete()
+                .uri("/employees/{id}" + savedEmployee.getId())
+                .exchange()
+                .expectStatus().isNoContent()
+                .expectBody(void.class );
+
+        webTestClient.delete()
+                .uri("/employees/{id}" + savedEmployee.getId())
+                .exchange()
+                .expectStatus().isNoContent();
+    }
 }
